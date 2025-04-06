5c93855d5ce74a78c32f74c38c0565969f2731e9
KerollesFathy <kerolles.f@outlook.com>
2025-04-06 14:19:52 +0530

    Added app gh link

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'axentorllc',
-      github: 'https://github.com/Axentorllc',
+      github: 'https://github.com/Axentorllc/financial_report_builder.git',
     },
     // github: {
     //   dir: '.starters/default/content',
