70f9cc14bcf68ef2afddf79a7d28a9429c0942ab
KerollesFathy <kerolles.f@outlook.com>
2025-04-06 16:04:13 +0530

    chore: Some Updates - added custom css file - added app logo in diff formats

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ export default defineAppConfig({
   docus: {
     title: 'Financial Report Builder',
     description: 'The Best Tool to Create Complex Financial Reports.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    // image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'axentorllc',
       github: 'https://github.com/Axentorllc/financial_report_builder.git',
